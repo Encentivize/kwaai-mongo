cfeb5bda403db6b27beed53fff66e1afa36e77a9
unknown <martin@MartinN-PC.entelect.local>
2014-09-24 13:07:04 +0200

    Version bump

--- testing.js
+++ testing.js
@@ -1,9 +1,31 @@
 var kwaaiMongo=require('./lib/mongo.js');
+var mquery=require("mquery");
+var mongo=require("mongodb");
+
+
+var t = new mongo.ObjectID();
+var isvalid=mongo.ObjectID.isValid(t);
 
 
 var connectionString="mongodb://127.0.0.1:27017/testdb";
 
+function collectionConnected(err,collection){
+    if(err){console.log(err)}
+    console.log("success connecting to single collection")
 
+    var query=mquery(collection);
+    kwaaiMongo.buildQueryFromOptions(query,{"additionalFields.val1":1})
+    query.exec(function(err,values){
+        console.log(err);
+        console.log(values);
+
+    })
+
+}
+kwaaiMongo.connectToCollection("testcol1",connectionString,collectionConnected)
+
+
+/*
 function collectionConnected(err,collection){
     if(err){console.log(err)}
     console.log("success connecting to single collection")
@@ -12,6 +34,12 @@ function collectionConnected(err,collection){
     collection.insert(document, function(err, records){
         console.log("Record added as "+records[0]._id);
     });
+
+
+    var query=mquery(collection);
+    kwaaiMongo.buildQueryFromOptions(query,{"additionalFields.val1":1})
+    query.exec
+
 }
 kwaaiMongo.connectToCollection("testcol1",connectionString,collectionConnected)
 
@@ -27,3 +55,5 @@ function collectionsConnected(err,collections){
     });}
 }
 kwaaiMongo.connectToCollections(connectionString,collectionsConnected,"testcol1","testcol2","testcol4","testcol5","testcol6","testcol7","testcol8","testcol9")
+
+    */
